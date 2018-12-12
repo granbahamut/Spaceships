1bfe9b2242ee0e23ac48350bfb1f0e958d413dde
Ivan Dario Pinilla <ivan.dario.pinilla@gmail.com>
2018-12-12 18:50:20 -0500

    Primera nave básica
    Esta es la primera nave que se mueve y dispara.

---
+++ Space ships/Assets/Scripts/NormalShot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalShot : MonoBehaviour {
+
+    public float speed;
+    public float dispersionRange;
+    private float dispersion;
+    private Rigidbody2D rb2d;
+    public float range;
+
+    private void Awake() {
+        Destroy(gameObject, range);
+    }
+
+    // Use this for initialization
+    void Start() {
+        dispersion = Random.Range(-dispersionRange, dispersionRange);
+        Vector2 v2 = new Vector2(dispersion, 1);
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(v2 * speed, ForceMode2D.Force);
+    }
+}
---
+++ Space ships/Assets/Scripts/ShipCannonFire.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipCannonFire : MonoBehaviour {
+
+    public float fireRate;
+    private float nextFire;
+    public List<GameObject> shot;
+    public Transform shotSpawn;
+    public int activeWeaponIndex;
+    private KeyCode[] keyCodes = {
+        KeyCode.Alpha1,
+        KeyCode.Alpha2,
+        KeyCode.Alpha3,
+        KeyCode.Alpha4,
+        KeyCode.Alpha5,
+        KeyCode.Alpha6,
+        KeyCode.Alpha7,
+        KeyCode.Alpha8,
+        KeyCode.Alpha9
+    };
+
+    // Update is called once per frame
+    void Update () {
+        for (int i = 0; i < keyCodes.Length; i++) {
+            if (Input.GetKeyDown(keyCodes[i])) {
+                activeWeaponIndex = i;
+                if(activeWeaponIndex + 1 > shot.Capacity) {
+                    activeWeaponIndex = shot.Capacity - 1;
+                }
+            }
+        }
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextFire) {
+            nextFire = Time.time + fireRate;
+            if (activeWeaponIndex >= 0) {
+                Instantiate(shot[activeWeaponIndex], shotSpawn.position, shotSpawn.rotation);
+            }
+            
+        }
+    }
+}
